c4385a44f8de6d0cb3dc43e12e55f7178edbbf17
Alana Gorukanti <52054877+alana170@users.noreply.github.com>
2024-10-19 19:35:35 -0400

    Create StringScheme.swift

---
+++ ios/src/pages/constants/StringScheme.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+struct StringConstants {
+
+  static let k: String = ""
+  static let kHttpFail: String = "HTTP request failed"
+  static let kLblAlertPolice: String = "Alert police"
+  static let kLblCarpool: String = "Carpool"
+  static let kUnableToFetch: String = "Unable to fetch data"
+  static let kLblReportIssue: String = "Report issue "
+  static let kInvalidUrl: String = "Invalid URL"
+  static let kLblSafety: String = "Safety"
+  static let kLblRoutes: String = "Routes"
+  static let kLblLeastDanger: String = "Least danger"
+  static let kLblMostDanger: String = "Most danger"
+
+}
