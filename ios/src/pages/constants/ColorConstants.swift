3e985244e88ff0b19ee139a21f668fc4c252a86c
Alana Gorukanti <52054877+alana170@users.noreply.github.com>
2024-10-19 19:35:06 -0400

    Create ColorConstants.swift

---
+++ ios/src/pages/constants/ColorConstants.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+struct ColorConstants {
+
+  static let Black900: Color = Color("Black900")
+  static let BlueA20033: Color = Color("BlueA20033")
+  static let BlueA2004c: Color = Color("BlueA2004c")
+  static let Red300: Color = Color("Red300")
+  static let BlueGray90087: Color = Color("BlueGray90087")
+  static let DeepPurpleA200: Color = Color("DeepPurpleA200")
+  static let WhiteA700: Color = Color("WhiteA700")
+  static let RedA700: Color = Color("RedA700")
+  static let Gray100: Color = Color("Gray100")
+
+}
